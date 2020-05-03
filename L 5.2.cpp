64c05d391389542d7c4da73e7d42a6cb07fc137c
vacalatyvika02@gmail.com <vacalatyvika02@gmail.com>
2020-05-03 23:31:56 +0300

    Добавьте файлы проекта.

---
+++ L 5.2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+/*
+ Дан файл f, компоненты которого являються целыми числами.
+Получить файл g, образованный из файла f исключением повторных.
+*/
+
+int main()
+{
+    ifstream f("f.txt");
+    set<int> numbers;
+    int temp;
+    while (f >> temp) numbers.insert(temp);
+    ofstream g("g.txt");
+    for (auto number : numbers) g << number << endl;
+
+    cout << "Open the 'g' file to see the result!" << endl;
+
+    return 0;
+}
